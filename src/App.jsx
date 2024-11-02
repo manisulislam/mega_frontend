053996f5cd1720c85fb1b8dff72078e084a19c6b
anisul islam <anisulislam980340@gmail.com>
2024-11-02 23:00:12 +0600

    appwrite backend setup completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  // console.log(import.meta.env.VITE_NAME)
+
+  return (
+    <>  
+      <h1>mega project in app write</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf={
+    appwriteUrl:String(import.meta.env.APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.APPWRITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.APPWRITE_DATABASE_ID),
+    appwriteCollectionId:String(import.meta.env.APPWRITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.APPWRITE_BUCKET_ID),
+    jwtSecret:String(import.meta.env.JWT_SECRET)
+}
+
+export default conf;
